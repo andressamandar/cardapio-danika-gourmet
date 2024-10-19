892b26bc0acbc220aab6272849ac08e1da1b04a5
Andressa Morais <andressa-am@hotmail.com>
2024-10-19 12:15:25 -0300

    add: adicionado css para nao mudar a cor e adicionado aba pães

--- src/app/components/cardapio/cardapio.component.ts
+++ src/app/components/cardapio/cardapio.component.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatTabsModule } from '@angular/material/tabs';
 
 @Component({
   selector: 'app-cardapio',
@@ -9,27 +10,26 @@ import { MatPaginatorModule } from '@angular/material/paginator';
   imports: [
     MatPaginatorModule, 
     MatButtonModule, 
-    CommonModule
+    CommonModule,
+    MatTabsModule,
   ],
   templateUrl: './cardapio.component.html',
   styleUrls: ['./cardapio.component.css'],
 })
 export class CardapioComponent {
-  mostrarProdutos: boolean = true;
-  mostrarIngredientes: boolean = false;
+  mostrarProdutos: boolean = true;  // Controla a exibição das abas (Bolos e Tortas, Pães)
+  mostrarIngredientes: boolean = false;  // Controla a exibição das marcas usadas
 
+  // Alterna para mostrar apenas as marcas usadas
   alternarMarcas(): void {
-    this.mostrarIngredientes = !this.mostrarIngredientes;
-    if (this.mostrarIngredientes) {
-      this.mostrarProdutos = false;
-    }
+    this.mostrarIngredientes = true;  // Exibe as marcas
+    this.mostrarProdutos = false;     // Oculta o cardápio (abas)
   }
 
+  // Alterna para mostrar o cardápio (abas Bolos e Tortas, Pães)
   alternarProdutos(): void {
-    this.mostrarProdutos = !this.mostrarProdutos;
-    if (this.mostrarProdutos) {
-      this.mostrarIngredientes = false;
-    }
+    this.mostrarProdutos = true;      // Exibe o cardápio (abas)
+    this.mostrarIngredientes = false; // Oculta as marcas
   }
 
   openWhatsApp() {
